c1ab7b3c05c2d130cdcb1e703f549e014db5d920
Diego Barroso <info@diegobarroso.es>
2022-12-02 07:34:27 +0100

    Refactor challenge 1

--- challenge01/challenge01.js
+++ challenge01/challenge01.js
@@ -1,18 +1,9 @@
 function wrapping(gifts) {
-  /* return gifts.map(gift => 
+  return gifts.map(gift => 
     '*'.repeat(gift.length + 2) +
     `\n*${gift}*\n` +
-    '*'.repeat(gift.length + 2)) */
-  const output = []
-  gifts.forEach(gift => {
-    output.push('*'.repeat(gift.length + 2) +
-    `\n*${gift}*\n` +
     '*'.repeat(gift.length + 2))
-  });
-    
-  return output
-  }
-
+}
 
 
 const gifts = ['book', 'game', 'socks']
