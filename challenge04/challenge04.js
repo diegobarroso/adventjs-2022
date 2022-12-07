9a98262ec357ae5d163ca31faba71ee2961dd1e3
Diego Barroso <info@diegobarroso.es>
2022-12-07 15:19:59 +0100

    Add challenge 4 solution without sorting array

--- challenge04/challenge04.js
+++ challenge04/challenge04.js
@@ -4,3 +4,15 @@ export function fitsInOneBox (boxes) {
       ? true
       : b.l < boxes[index + 1].l && b.w < boxes[index + 1].w && b.h < boxes[index + 1].h)
 }
+
+/* Solution without sorting array
+ * while (boxes.length > 1) {
+    const lastBox = boxes.pop()
+    if (!boxes.every(box =>
+        (lastBox.l < box.l && lastBox.w < box.w && lastBox.h < box.h) ||
+        (lastBox.l > box.l && lastBox.w > box.w && lastBox.h > box.h)
+        ))
+    return false
+  }
+  return true
+ */
